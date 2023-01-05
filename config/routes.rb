52a0319446446aa2063c91dccda129139e27632f
Thiago <tlcrezende@gmail.com>
2023-01-05 04:18:24 -0300

    Set the routes contraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespeace :api, defaults: { format: :json } do
+    scope module: :v1, constraints: ApiConstraints.new(versio: 1, default: true) do
+      # route
+    end
 end
