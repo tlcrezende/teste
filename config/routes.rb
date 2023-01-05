55c0d3098d4f996dcb805e4304e46f732a99d2ea
Thiago <tlcrezende@gmail.com>
2023-01-05 04:36:33 -0300

    Fix bug routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
-  namespeace :api, defaults: { format: :json } do
-    scope module: :v1, constraints: ApiConstraints.new(versio: 1, default: true) do
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # route
     end
+  end
 end
