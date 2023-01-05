024ae3b68d934db9ff9e746f442f3b6a3439416e
Thiago <tlcrezende@gmail.com>
2023-01-05 17:28:10 -0300

    Add enum to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,19 @@ class Book < ApplicationRecord
 
   has_many :user_books, dependent: :destroy
   has_many :users, through: :user_books
+  has_many :reviews, dependent: :destroy
+
+  enum genre: {
+    uninformed: 0,
+    adventure: 1,
+    classics: 2,
+    crime: 3,
+    fairy_tales: 4,
+    fantasy: 5,
+    historical_fiction: 6,
+    horror: 7,
+    humour_satire: 9,
+    other: 10
+	}
+
 end
