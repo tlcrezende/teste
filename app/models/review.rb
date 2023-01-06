018d18abb05e4c1c01e671f14ae5c36e0d1a2650
Thiago <tlcrezende@gmail.com>
2023-01-05 22:04:11 -0300

    Add validates numericality in score

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   validates :user_review, :book_id, :score, presence: true 
+  validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 
   belongs_to :user
   belongs_to :book
