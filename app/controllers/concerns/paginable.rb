9f876215fbe1c2f72b3daf9bd2016905a80eae3f
Thiago <tlcrezende@gmail.com>
2023-01-07 09:17:44 -0300

    Fix bug comparion with nil

--- app/controllers/concerns/paginable.rb
+++ app/controllers/concerns/paginable.rb
@@ -3,12 +3,14 @@ module Paginable
   
   # current_page por default é 1 e assumirar valores do parâmetro se ele for positivo 
   def current_page
-    (params[:page] & (params[:page] > 0) ? params[:page] : 1).to_i 
+    return 1 unless params[:page].present?
+    return (params[:page].to_i > 0 ? params[:page] : 1)
   end
   
   # per_page por default é 6 e assumirar valores do parâmetro se ele for positivo 
   def per_page
-    (params[:per_page] & (params[:per_page] > 0) ? params[:per_page] : 6).to_i
+    return 6 unless params[:per_page].present?
+    return (params[:per_page].to_i > 0 ? params[:per_page] : 1)
   end
   
   # Retorno padrão de paginação 
