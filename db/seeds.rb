072d00fc8099c039be3d5ac04973c3f7bd1fc1fa
Thiago <tlcrezende@gmail.com>
2023-01-07 14:13:42 -0300

    Add db seed to populate prod db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,25 @@
 30.times do
   FactoryBot.create :book
 end
+
+9.times do
+  FactoryBot.create :user
+end
+
+60.times do
+  userbook = UserBook.new(user_id: Faker::Number.between(from: 1, to: 10), book_id: Faker::Number.between(from: 1, to: 60))
+  userbook.save
+end
+
+100.times do
+  review = Review.new(
+    user_review: Faker::Lorem.sentence(word_count: 12),
+    score: Faker::Number.between(from: 1, to: 10),
+    user_id: Faker::Number.between(from: 1, to: 10),
+    book_id: Faker::Number.between(from: 1, to: 60)
+  )
+  review.save
+end
+
+
+
