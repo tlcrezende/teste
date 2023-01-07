4e9029cdd74ba293df5fe0b3bd76aa6fd4b6ff72
Thiago <tlcrezende@gmail.com>
2023-01-07 08:57:51 -0300

    Add condition and default value to current page and per_page

--- app/controllers/concerns/paginable.rb
+++ app/controllers/concerns/paginable.rb
@@ -1,14 +1,17 @@
 module Paginable
   protected
   
+  # current_page por default é 1 e assumirar valores do parâmetro se ele for positivo 
   def current_page
-    (params[:page] || 1).to_i
+    (params[:page] & (params[:page] > 0) ? params[:page] : 1).to_i 
   end
   
+  # per_page por default é 6 e assumirar valores do parâmetro se ele for positivo 
   def per_page
-    (params[:per_page] || 6).to_i
+    (params[:per_page] & (params[:per_page] > 0) ? params[:per_page] : 6).to_i
   end
   
+  # Retorno padrão de paginação 
   def meta_attributes(collection, extra_meta = {})
     {
       current_page: collection.current_page,
