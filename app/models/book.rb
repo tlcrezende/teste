ff90d00288f6e401b9b87e94a1cdef3dcbd639ca
Thiago <tlcrezende@gmail.com>
2023-01-07 09:18:02 -0300

    Add condition in search_by_genre

--- app/models/book.rb
+++ app/models/book.rb
@@ -18,8 +18,11 @@ class Book < ApplicationRecord
     other: 9
 	}
 
+  # Pesquisa por título e autor 
   scope :search_by_title, -> (search_by_title) { where('LOWER(title) LIKE ?', "%#{search_by_title.downcase}%") if search_by_title.present? }
   scope :search_by_author, -> (search_by_author) { where('LOWER(author) LIKE ?', "%#{search_by_author.downcase}%") if search_by_author.present? }
-  scope :search_by_genre, -> (search_by_genre) { where('genre = ?', "#{search_by_genre}") if search_by_genre.present? }
+  
+  # Pesquisa por gênero, bloqueando gênero que não estão no enum genre e retornando uninformed 
+  scope :search_by_genre, -> (search_by_genre) { where('genre = ?', "#{Book.genres[search_by_genre].nil? ? 0 : Book.genres[search_by_genre]}") if search_by_genre.present? }
 
 end
